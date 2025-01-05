bc6298cef227abe6107bdcc36692b1d412ffb204
akshjwshs <kaleemmods4@gmail.com>
2025-01-05 12:29:55 +0500

    Update and rename config.js to Deploy.js
    Signed-off-by: akshjwshs <kaleemmods4@gmail.com>

--- Deploy.js
+++ Deploy.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "quxFXDSK#CFkVmbLYTxUNUxr0-rzjaYwUXQMOm56GoxvDjOJUhdg",
     CAPTION: process.env.CAPTION || "POWERED BY MAFIA-ADEEL🌹",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
